a6cff4744fbd632480b2cafba4cc3ca4aa6fa2d8
ESOS Lab <esoslab@gmail.com>
2017-03-23 13:15:58 +0900

    fix_hu
    Eliminate duplicate calculations

--- Hu_kpks.m
+++ Hu_kpks.m
@@ -4,10 +4,13 @@ kpks = zeros(1,SizeofCal);
 fac(1)=1;
 fac(2)=1;
 for fa=3:1:Np+1;
-    %fac(fa) = fac(fa-1) * (fa-1);
-    fac(fa) = sqrt(2*pi*(fa-1)) * ((fa-1)/exp(1))^(fa-1);
+    fac(fa) = fac(fa-1) * (fa-1);
+    %fac(fa) = sqrt(2*pi*(fa-1)) * ((fa-1)/exp(1))^(fa-1);
 end
 
+
+hj = (t-r-u).*Np;  %(max(0 , Np.*(t-r-jj-1) - u*Np*((1-(1/(u*Np)))^((jj+1)*Np)) ) );
+pj = ( (1-(1/(u*Np))).^hj );
 
 
 for k=1:1:Np-1;
@@ -16,45 +19,56 @@ for k=1:1:Np-1;
 
     for jj=0:1:s-1;
         spjm1 = zeros(1,SizeofCal);
-        hj1 = (t-r-u).*Np;%(max(0 , Np.*(t-r-jj-1) - u*Np*((1-(1/(u*Np)))^((jj+1)*Np)) ) );%
-        pj1 = ( (1-(1/(u*Np))).^hj1 );
+        spjm2 = zeros(1,SizeofCal);
+        %hj1 = (t-r-u).*Np;  %(max(0 , Np.*(t-r-jj-1) - u*Np*((1-(1/(u*Np)))^((jj+1)*Np)) ) );
+        %pj1 = ( (1-(1/(u*Np))).^hj1 );
         %h1(1,jj+1)=hj1;
         %p1(1,jj+1)=pj1;
 
         for m=0:1:k-1;
-            spjm1 = spjm1 + ( ( (fac(Np+1) ) / (fac(m+1) * fac(Np-m+1) ) ) .* (pj1.^m) .* ((1-pj1).^(Np-m)) );
+            spjm1 = spjm1 + ( ( (fac(Np+1) ) / (fac(m+1) * fac(Np-m+1) ) ) .* (pj.^m) .* ((1-pj).^(Np-m)) );
+            %spjm1 = ( ( (fac(Np+1) ) / (fac(m+1) * fac(Np-m+1) ) ) .* (pj.^m) .* ((1-pj).^(Np-m)) );
             %s1(jj+1,m+1)=spjm1;
         end
+            spjm2 = spjm1 + ( ( (fac(Np+1) ) / (fac(k+1) * fac(Np-k+1) ) ) .* (pj.^k) .* ((1-pj).^(Np-k)) );        
         
         if jj==0
-            pajvjk1 = 1-spjm1;
+            pajvjk1 = 1-(spjm1);
+            pajvjk2 = 1-(spjm2);
         else
-            pajvjk1 = pajvjk1.*(1-spjm1);
+            pajvjk1 = pajvjk1.*(1-(spjm1));
+            pajvjk2 = pajvjk2.*(1-(spjm2));
         end
         %pa1(1,jj+1)=pajvjk1;
     end
+    
 
+    %{
     for jj=0:1:s-1;
         spjm2 = zeros(1,SizeofCal);
-        hj2 = (t-r-u)*Np;%(max(0 , Np.*(t-r-jj-1) - u*Np*((1-(1/(u*Np)))^((jj+1)*Np)) ) );%
-        pj2 = ( (1-(1/(u*Np))).^hj2 );
+        %spjm2 = zeros(1,k);
+        %hj2 = (t-r-u)*Np;   %(max(0 , Np.*(t-r-jj-1) - u*Np*((1-(1/(u*Np)))^((jj+1)*Np)) ) );
+        %pj2 = ( (1-(1/(u*Np))).^hj2 );
         %h2(1,jj+1)=hj2;
         %p2(1,jj+1)=pj2;
 
         for m=0:1:k;
-            spjm2 = spjm2 + ( ( (fac(Np+1) ) / (fac(m+1) * fac(Np-m+1) ) ) .* (pj2.^m) .* ((1-pj2).^(Np-m)) );
+            spjm2 = spjm2 + ( ( (fac(Np+1) ) / (fac(m+1) * fac(Np-m+1) ) ) .* (pj.^m) .* ((1-pj).^(Np-m)) );
+            %spjm2(m+1) = ( ( (fac(Np+1) ) / (fac(m+1) * fac(Np-m+1) ) ) .* (pj.^m) .* ((1-pj).^(Np-m)) );
             %s2(jj+1,m+1)=spjm2;
         end
         
         if jj==0
-            pajvjk2 = 1-spjm2;
+            pajvjk2 = 1-(spjm2);
         else
-            pajvjk2 = pajvjk2.*(1-spjm2);
+            pajvjk2 = pajvjk2.*(1-(spjm2));
         end
        % pa2(1,jj+1)=pajvjk2;
     end
+    %}
     
     kpks = kpks+(k.*(pajvjk1-pajvjk2));
+    %kpks(k) = (k.*(pajvjk1-pajvjk2));
     %kp(1,k);
 end
-A_cc=Np./(Np-kpks);
+A_cc=Np./(Np-(kpks));
